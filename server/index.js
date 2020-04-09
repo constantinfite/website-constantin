5e67f5fdd26a87b3dddc94f74cb147423cc0cb53
constantinfite <57963890+constantinfite@users.noreply.github.com>
2020-04-09 20:55:00 +0200

    server node

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const posts = require("./routes/api/posts");
+
+const app = express();
+const path = require("path");
+
+//Middleware
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/api/posts", posts);
+
+if (process.env.NODE_ENV === "production") {
+  //static folder
+  app.use(express.static(__dirname + "/public/"));
+  //HANDLE SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
